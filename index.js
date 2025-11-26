7b7641df356a4877ff94a4b0adb293bbfcc419f1
shubham781999 <138647879+shubham781999@users.noreply.github.com>
2025-11-27 00:57:17 +0530

    Fix message in lambda response for production

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ exports.handler = async (event) => {
         statusCode: 200,
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
-            message: "lambda pipeline is workinggggggggggfor prod with alias",
+            message: "lambda pipeline is for prod with alias",
             time: new Date().toISOString(),
         }),
     };
