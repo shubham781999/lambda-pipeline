7731732eddfe123bf572fc8c28e710eca4aa2152
shubham781999 <138647879+shubham781999@users.noreply.github.com>
2025-11-27 00:42:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ exports.handler = async (event) => {
         statusCode: 200,
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
-            message: "Hello from Lambda CI/CD Pipeline-testinggggggggg",
+            message: "Hello from Lambda CI/CD Pipeline-testingprodddddggggg",
             time: new Date().toISOString(),
         }),
     };
