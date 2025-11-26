14ce314eee88ae5671e7eda3158892975510b45b
shubham781999 <138647879+shubham781999@users.noreply.github.com>
2025-11-27 00:34:32 +0530

    Fix message in Lambda response

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ exports.handler = async (event) => {
         statusCode: 200,
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
-            message: "Hello from Lambda CI/CD Pipeline-testingggggggggggggggg",
+            message: "Hello from Lambda CI/CD Pipeline-testinggggggggg",
             time: new Date().toISOString(),
         }),
     };
